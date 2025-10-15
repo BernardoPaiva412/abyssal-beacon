aeca8ac65c5fa227bfdbad33b9a40aa070a151b8
bernardopaiva412 <bernardo.paiva@aluno.ifsp.edu.br>
2025-10-15 19:41:29 -0300

    protagonista funcional

--- js/jogo.js
+++ js/jogo.js
@@ -3,19 +3,42 @@ const config = {
     width: 800,
     height: 600,
     backgroundColor: '#151515',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
     }
 }
-const game = new Phaser.Game(config)
+let rob
+let tec
+const jog = new Phaser.Game(config)
 function preload() {
 
 }
 function create() {
-
+rob = this.physics.add.sprite(100, 300, null)
+rob.setCollideWorldBounds(true)
+tec = this.input.keyboard.createCursorKeys()
+rob.setTint(0x00ff00)
 }
 function update() {
-    
+    rob.setVelocity(0)
+    const vel = 200
+    if (tec.left.isDown) {
+        rob.setVelocityX(-vel)
+    } if (tec.right.isDown) {
+        rob.setVelocityX(vel)
+    }
+    if (tec.up.isDown) {
+        rob.setVelocityY(-vel)
+    } else if (tec.down.isDown) {
+        rob.setVelocityY(vel)
+    }
 }
